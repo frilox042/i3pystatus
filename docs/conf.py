06c1eeba578b7364dc0557f42aa4a693582bec87
Restyled.io <commits@restyled.io>
2020-04-12 15:50:17 +0000

    Restyled by autopep8

--- docs/conf.py
+++ docs/conf.py
@@ -11,13 +11,14 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import mock
+import sys
+import os
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 
 # requires PyPI mock
-import mock
 
 MOCK_MODULES = [
     "alsaaudio",
@@ -233,21 +234,21 @@ htmlhelp_basename = 'i3pystatusdoc'
 # -- Options for LaTeX output --------------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    # 'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    # 'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    # 'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'i3pystatus.tex', 'i3pystatus Documentation',
-   'Author', 'manual'),
+    ('index', 'i3pystatus.tex', 'i3pystatus Documentation',
+     'Author', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -290,9 +291,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'i3pystatus', 'i3pystatus Documentation',
-   'Author', 'i3pystatus', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', 'i3pystatus', 'i3pystatus Documentation',
+     'Author', 'i3pystatus', 'One line description of project.',
+     'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
@@ -364,6 +365,7 @@ epub_copyright = '2013, Author'
 
 # If false, no index is generated.
 #epub_use_index = True
+
 
 def setup(app):
     app.add_stylesheet('i3pystatus.css')
